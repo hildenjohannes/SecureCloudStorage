3f03dfce9574b17f743c7b89735b3ffb08465a28
hildenjohannes <johannes.hilden@gmail.com>
2017-03-03 16:08:54 +0100

    In progress of implementing a nice way to display an arbitrary number of objects

---
+++ Prototypes/WebFileOrg/haste-compiler/haste-compiler.hs
@@ -0,0 +1,30 @@
+-- | A simple example of using Haste.App: a "message trader".
+--   When the button is clicked, the user is asked to input a message. This
+--   message is then sent to the server, and the user in return receives the
+--   previous message left using this trade function.
+--
+--   This example demonstrates the basics of using Haste.App: how to maintain
+--   server state, and how to export and call server API functions.
+import Haste.App
+import qualified Haste.App.Concurrent as H
+import Haste.DOM
+import Haste.Events
+import qualified Control.Concurrent as C
+
+main :: IO ()
+main = do
+  runApp (mkConfig "localhost" 24601) $ do
+    remoteMsg <- liftServerIO $ C.newMVar "This is not a message."
+
+    trade <- remote $ \newmsg -> do
+      message <- remoteMsg
+      liftIO $ do oldmsg <- C.takeMVar message
+                  C.putMVar message newmsg
+                  return oldmsg
+
+    runClient $ withElem "button" $ \button -> do
+      button `onEvent` Click $ \_ -> do
+        newmsg <- prompt "Enter a message"
+        oldmsg <- onServer $ trade <.> newmsg
+        alert $ "The old message was: " ++ oldmsg
+      return ()
--- Prototypes/WebFileOrg/webFileOrg.hs
+++ Prototypes/WebFileOrg/webFileOrg.hs
@@ -1,7 +1,35 @@
-import Haste
+import Haste.App
+import qualified Haste.App.Concurrent as H
 import Haste.DOM
 import Haste.Events
-import Haste.Graphics.Canvas
+import Haste.Parsing
+import qualified Control.Concurrent as C
 
 main :: IO ()
-main = alert "Hello!"
+main = do
+    runApp (mkConfig "localhost" 24601) $ do
+
+      runClient $ withElem "button" $ \button -> do
+        button `onEvent` Click $ \_ -> do
+          --alert "hello"
+          --msg <- liftIO $ readFile "/home/johannes/examples.desktop"
+          --setAttr str
+          --alert "HEJ"
+          --japp <- newTextElem "Hejsan"
+          --appendChild japp "Filelist"
+          --japp <- withElem "str" $ \x -> do
+          msg <- withElem "inputAmount" $ \inputAmount -> getProp inputAmount "value"
+          msgInt <- runParser int msg
+          alert msg --FÖRSÖKER TA IN EN SIFFRA OCH LOOPA SÅ MÅNGA GÅNGER, GER MAYBE a, PROBLEM
+          liftIO $ fillList 3
+
+          --withElem "li5" $ \li5 -> setProp li5 "innerText" "hej"
+        return ()
+
+fillList :: Int -> IO ()
+fillList 0 = return ()
+fillList n = do
+               listItem <- newElem "li"
+               setProp listItem "innerText" "Filename.txt"
+               withElem "fileList" $ \fileList -> appendChild fileList listItem
+               fillList (n-1)
