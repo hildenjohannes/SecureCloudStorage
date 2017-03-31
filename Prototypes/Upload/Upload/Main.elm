06951a1d48514781ccf22ece1896ee87b6a941ea
sumsarj <racemouse95@gmail.com>
2017-03-31 10:38:00 +0200

    Initial commit

---
+++ Prototypes/Upload/Upload/Main.elm
@@ -0,0 +1,188 @@
+module Main exposing (..)
+
+import Html exposing (Html, div, input, button, h1, p, text, form)
+import Html.Attributes exposing (type_, id, style, multiple)
+import Html.Events exposing (onClick, on, onSubmit, onInput)
+import Task
+import Json.Decode as Json exposing (Value, andThen)
+import FileReader exposing (..)
+import Http exposing (..)
+import List as L
+
+
+
+type alias Files =
+    List NativeFile
+
+
+type alias Model =
+    { message : String
+    , selected : Files
+    , contents : List String
+    }
+
+
+init : Model
+init =
+    { message = "Waiting..."
+    , selected = []
+    , contents = []
+    }
+
+
+type Msg
+    = Upload
+    | FilesSelect Files
+    | FilesSelectUpload Files
+    | Submit
+    | FileData (Result FileReader.Error String)
+    -- | FileDataBinary (Result FileReader.Error FileContentArrayBuffer)
+    | PostResult (Result Http.Error Json.Value)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Upload ->
+            model ! List.map sendFileToServer model.selected
+
+        FilesSelect fileInstances ->
+            { model
+                | selected = fileInstances
+                , message = "Something selected"
+            }
+                ! []
+
+        FilesSelectUpload fileInstances ->
+            { model | selected = fileInstances } ! List.map readTextFile fileInstances
+
+        Submit ->
+            { model | message = Basics.toString model.selected } ! List.map readTextFile model.selected
+
+        FileData (Ok str) ->
+            { model | contents = str :: model.contents } ! []
+
+        FileData (Err err) ->
+            { model | message = FileReader.prettyPrint err } ! []
+        -- FileDataBinary (Ok data) ->
+        --     (model, Cmd.none)
+        PostResult (Ok msg) ->
+            { model | message = toString msg } ! []
+
+        PostResult (Err err) ->
+            { model | message = toString err } ! []
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div [ containerStyles ]
+        [ div []
+            [ h1 [] [ text "Single file select + Upload separate" ]
+            , input
+                [ type_ "file"
+                , onchange FilesSelect
+                ]
+                []
+            , button
+                [ onClick Upload ]
+                [ text "Read file" ]
+            ]
+        , div []
+            [ h1 [] [ text "Multiple files with automatic upload" ]
+            , input
+                [ type_ "file"
+                , onchange FilesSelectUpload
+                , multiple True
+                ]
+                []
+            ]
+        , form
+            [ onSubmit Submit ]
+            [ h1 [] [ text "Form with submit button" ]
+            , input
+                [ type_ "file"
+                , onchange FilesSelect
+                , multiple True
+                ]
+                []
+            , button
+                [ type_ "submit" ]
+                [ text "Submit" ]
+            ]
+        , div []
+            [ h1 [] [ text "Results" ]
+            , p []
+                [ text <|
+                    "Files: "
+                        ++ commaSeperate (List.map .name model.selected)
+                ]
+            , p []
+                [ text <|
+                    "Contents: "
+                        ++ commaSeperate model.contents
+                ]
+            , div [] [ text model.message ]
+            ]
+        ]
+
+
+commaSeperate : List String -> String
+commaSeperate lst =
+    List.foldl (++) "" (List.intersperse ", " lst)
+
+
+onchange action =
+    on
+        "change"
+        (Json.map action parseSelectedFiles)
+
+
+containerStyles =
+    style [ ( "padding", "20px" ) ]
+
+
+
+-- TASKS
+
+
+readTextFile : NativeFile -> Cmd Msg
+readTextFile fileValue =
+    readAsTextFile fileValue.blob
+        |> Task.attempt FileData
+--
+-- readArrayBuffer : NativeFile -> Cmd Msg
+-- readArrayBuffer fileValue =
+--     readArrayBuffer fileValue.blob
+--         |> Task.attempt FileDataBinary
+
+-- |> Task.map Ok
+-- |> Task.onError (Task.succeed << Err)
+-- |> Task.perform FileData
+
+sendFileToServer : NativeFile -> Cmd Msg
+sendFileToServer buf =
+    let
+        body =
+            Http.multipartBody
+                [ FileReader.filePart "simtest" buf,
+                stringPart "part1" "42"
+                
+                ]
+    in
+        Http.post "http://localhost:5000/upload" body Json.value
+            |> Http.send PostResult
+
+-- ----------------------------------
+
+
+main =
+    Html.program
+        { init = ( init, Cmd.none )
+        , update = update
+        , view = view
+        , subscriptions = (always Sub.none)
+        }
