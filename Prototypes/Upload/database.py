b3f6af5c99a171db29c79f34bfae1d10d01141bb
sumsarj <racemouse95@gmail.com>
2017-04-18 10:37:09 +0200

    changed name on db.py to database.py

---
+++ Prototypes/Upload/database.py
@@ -0,0 +1,50 @@
+from peewee import *
+
+db = MySQLDatabase('scsdb', user='scs',passwd='scs123')
+
+class BaseModel(Model):
+    class Meta:
+        database=db
+
+class Usermeta(BaseModel):
+    fnamn = CharField(max_length=20)
+    enamn = CharField(max_length=20)
+    email = CharField(unique=True, max_length=50)
+    pw    = CharField(max_length=50)
+
+    def userLogin(params):
+        for auth in Usermeta.filter(email=params[0]):
+            if auth.pw == params[1]:
+                return True
+            else:
+                return False
+        return False
+    def userRegister(params):
+        #todo: check so that email isn't already taken.
+        try:
+            with db.atomic():
+                User.create(fnamn=params[0], enamn=params[1], email=params[2], pw=params[3])
+        except peewee.IntegrityError:
+                return False
+        return True
+
+#Usermeta.create_table()
+#user = Usermeta(fnamn='Test', enamn='Testsson', email='test@chalmers.se', pw='testpw')
+#user.save()
+
+# for user in Usermeta.filter(email="test@chalmers.se"):
+#     print (user.fnamn)
+#
+# #Usermeta.userRegister("Rasmus","Johansson","rasmus@chalmers.se", "rasmus")
+# a = Usermeta.userLogin("rasmus@chalmers.se","rasmus")
+# print (a)
+
+#class Userfiles(BaseModel)
+#
+# class Teams(BaseModel):
+#
+#
+# class Filemeta(BaseModel):
+#     name=CharField(max_length=50)
+#     owner=ForeignKeyField(Usermeta)
+#     sharedWith=ForeignKeyField(Usermeta, related_name='sharedwith')
