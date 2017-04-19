4921623222e73e81d816de1ba9dbfd474d1d0cff
sumsarj <racemouse95@gmail.com>
2017-04-19 13:09:57 +0200

    updated the initdatabase script

--- nonModular/initdatabasescript.py
+++ nonModular/initdatabasescript.py
@@ -1,4 +1,9 @@
 from database import Usermeta
-Usermeta.create_table()
-user = Usermeta(fnamn='Test', enamn='Testsson', email='test@chalmers.se', pw='testpw')
-user.save()
+
+try:
+    Usermeta.create_table()
+    user = Usermeta(fnamn='Test', enamn='Testsson', email='test@chalmers.se', pw='testpw')
+    user.save()
+    print("Table usermeta has been created and a test row has been inserted!")
+except:
+    print("Something went wrong, check that the database exists: database = scsdb, user = scs, password = scs123")
