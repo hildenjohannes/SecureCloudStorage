af94c47d16aeee3ca02750aa1951d3d4e2b5ff56
sumsarj <racemouse95@gmail.com>
2017-04-28 14:22:34 +0200

    Added database.py and updated server.py

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,8 @@
 from tornado import httpserver, websocket, web, ioloop
-
+from database import Usermeta, Filemeta
+json='["fakeFile0", "fakeFile1"]'
 class SocketHandler(websocket.WebSocketHandler):
+    user = ""
     def check_origin(self, origin):
         return True
 
@@ -8,15 +10,36 @@ class SocketHandler(websocket.WebSocketHandler):
         pass
 
     def on_message(self, message):
+        global user
         params = message.split("|")
         method = params.pop(0)
         if method == "login":
-            self.write_message(str(self.login(params)))
+            #self.write_message(str(self.login(params)))
+            if (Usermeta.userLogin(params)):
+                user = params[0]
+                print(user)
+                self.write_message('True')
+
         elif method == "upload":
-            print(params[0])
-            print(params[1])
+            print("wooohoo UPLOAD")
+            filee = open(params[0], 'w+')
+            filee.write(params[1])
+            if Filemeta.addFile([params[0],'100',Usermeta.filter(email=user)]):
+                print("filemeta added!")
+            #print(params[0])
+            #print(params[1])
+        elif method == "register":
+            self.write_message(str(Usermeta.userRegister(params)))
         elif method == "listFiles":
-            self.write_message(json) # TODO: get actual files
+            mes='['
+            for userr in Usermeta.filter(email=user):
+                for filee in userr.files:
+                    mes = mes + '"' + str(filee.name) + '",'
+
+            mes = mes[:-1] + ']'
+            print(mes)
+            self.write_message(mes) # TODO: get actual files
+            mes=''
         else:
             self.write_message("Invalid argument")
 
